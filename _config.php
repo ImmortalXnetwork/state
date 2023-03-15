8b6d9e1981ae5cd38b7b2ec7b3df1061a622fc23
Shashankk <106220488+shashankktiwariii@users.noreply.github.com>
2023-03-15 09:10:40 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "http://anikatsu.shashanktiwar11.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
