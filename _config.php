31343630222c4607b154fc224f68d07a1dce7e45
Mohammed suhail <119152757+ImmortalXnetwork@users.noreply.github.com>
2024-04-01 14:25:25 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
-<?php $servername = "localhost"; $username = "animehin_root"; $password = "Fazal@2002"; $dbname = "animehin_fazal"; // Create connection $conn = new mysqli($servername, $username, $password, $dbname); // Check connection if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); } echo "Connected successfully";
+<?php $servername = "localhost"; $username = "animehin_root"; $password = "Fazal@2002"; $dbname = "animehin_fazal"; // Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname); // Check connection if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); } echo "Connected successfully";
 
 $websiteTitle = "AnimeState"; // Website Name
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
